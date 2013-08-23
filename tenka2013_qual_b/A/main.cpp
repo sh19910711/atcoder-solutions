a1717c3b1bfaccebae5398359e7d1dda5d188e1a
Hiroyuki Sano <sh19910711@gmail.com>
2013-08-23 18:07:22 +0900

    tenka 2013 qual b - A: Accepted

---
+++ tenka2013_qual_b/A/main.cpp
@@ -0,0 +1,175 @@
+
+// @snippet<sh19910711/contest:headers.cpp>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <numeric>
+#include <limits>
+#include <complex>
+#include <functional>
+#include <iterator>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+
+// @snippet<sh19910711/contest:solution/interface.cpp>
+namespace solution {
+  class SolutionInterface {
+  public:
+    virtual int run() = 0;
+    
+  protected:
+    SolutionInterface() {}
+    virtual void pre_calc() {}
+    virtual bool action() = 0;
+    virtual void init() {};
+    virtual bool input() { return false; };
+    virtual void output() const {};
+  };
+}
+
+// @snippet<sh19910711/contest:solution/solution-base.cpp>
+namespace solution {
+  class SolutionBase: public SolutionInterface {
+  public:
+    virtual int run() {
+      this->pre_calc();
+      while ( this->action() );
+      return 0;
+    }
+    
+  };
+}
+
+// @snippet<sh19910711/contest:storage/vector.cpp>
+namespace storage {
+  template <typename ValueType, int SIZE> class VectorClass {
+  public:
+    typedef ValueType Type;
+    VectorClass() { data.resize(SIZE); }
+    Type& operator [] ( const int& index ) { return data[index]; }
+    const Type& operator [] ( const int& index ) const { return data[index]; }
+
+    typename std::vector<Type>::iterator at( const int& index ) {
+      return data.begin() + index;
+    }
+
+  private:
+    std::vector<Type> data;
+  };
+}
+
+// @snippet<sh19910711/contest:storage/vector_types.cpp>
+namespace storage {
+  const int VECTOR_NONE = -1;
+  template <typename ValueType, int SIZE_X = VECTOR_NONE, int SIZE_Y = VECTOR_NONE, int SIZE_Z = VECTOR_NONE> struct Vector {
+    typedef storage::VectorClass<ValueType, SIZE_X> Type__;
+    typedef storage::VectorClass<Type__, SIZE_Y> Type_;
+    typedef storage::VectorClass<Type_, SIZE_Z> Type;
+  };
+  template <typename ValueType, int SIZE_X, int SIZE_Y> struct Vector<ValueType, SIZE_X, SIZE_Y, VECTOR_NONE> {
+    typedef storage::VectorClass<ValueType, SIZE_X> Type_;
+    typedef storage::VectorClass<Type_, SIZE_X> Type;
+  };
+  template <typename ValueType, int SIZE_X> struct Vector<ValueType, SIZE_X, VECTOR_NONE, VECTOR_NONE> {
+    typedef storage::VectorClass<ValueType, SIZE_X> Type;
+  };
+}
+
+// @snippet<sh19910711/contest:solution/typedef.cpp>
+namespace solution {
+  typedef std::istringstream ISS;
+  typedef std::ostringstream OSS;
+  typedef long long Int;
+  typedef long double Double;
+  typedef std::string String;
+  
+}
+
+// @snippet<sh19910711/contest:solution/namespace-area.cpp>
+namespace solution {
+  // namespaces, types
+  using namespace std;
+  
+}
+
+// @snippet<sh19910711/contest:solution/consts-area.cpp>
+namespace solution {
+  // constant vars
+  const int SIZE = 100;
+}
+
+// @snippet<sh19910711/contest:solution/storages-area.cpp>
+namespace solution {
+  // storages
+  typedef storage::Vector<String, SIZE>::Type Words;
+  Words D;
+  int DC;
+
+  String result;
+}
+
+// @snippet<sh19910711/contest:solution/solver-area.cpp>
+namespace solution {
+  class Solver {
+  public:
+    void solve() {
+      std::sort(D.at(0), D.at(DC));
+      for ( int i = 0; i < DC; ++ i )
+        std::cout << D[i] << std::endl;
+      result = D[6];
+    }
+  protected:
+    
+  };
+}
+
+// @snippet<sh19910711/contest:solution/solution.cpp>
+namespace solution {
+  class Solution: public SolutionBase {
+  public:
+  protected:
+    virtual bool action() {
+      this->init();
+      if ( ! this->input() )
+        return false;
+      solver.solve();
+      this->output();
+      return true;
+    }
+
+    bool input() {
+      DC = 0;
+      if ( ! ( std::cin >> D[DC] ) )
+        return false;
+      DC ++;
+      while ( std::cin >> D[DC] )
+        DC ++;
+      return true;
+    }
+
+    void output() {
+      std::cout << result << std::endl;
+    }
+    
+    Solver solver;
+  };
+}
+
+// @snippet<sh19910711/contest:main.cpp>
+#ifndef __MY_UNIT_TEST__
+int main() {
+  std::cin.sync_with_stdio(false);
+  return solution::Solution().run();
+}
+#endif
+
+
